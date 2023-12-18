e2be1c45ebf9f43ac883914b00cd055ec09ed881
omniavincit98 <jeancc1532@gmail.com>
2023-12-17 21:42:55 -0300

    Configuring cors

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: '*',
+    origin: 'https://oftalmoreact.000webhostapp.com',
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }))
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://oftalmoreact.000webhostapp.com/")
+    res.setHeader("Access-Control-Allow-Origin", "https://oftalmoreact.000webhostapp.com")
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
