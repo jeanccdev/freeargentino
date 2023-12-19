b291ebcf99ed2e01c98fbe0a35d5a5d96bb71f43
omniavincit98 <jeancc1532@gmail.com>
2023-12-19 08:45:39 -0300

    Configuring db connection

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 import { Sequelize } from 'sequelize'
 import mysql from 'mysql2'
 
-const dbname = process.env.DB_DBNAME
-const username = process.env.DB_USERNAME
-const password = process.env.DB_PASSWORD
-const hostname = process.env.DB_HOST
+// const dbname = process.env.DB_DBNAME
+// const username = process.env.DB_USERNAME
+// const password = process.env.DB_PASSWORD
+// const hostname = process.env.DB_HOST
 
-const sequelize = new Sequelize(dbname, username, password, {
-    host: hostname,
+const sequelize = new Sequelize('wadswo11_documentos', 'wadswo11_documentos', 'Gil93nCZLezu', {
+    host: 'br1020.hostgator.com.br',
     dialect: 'mysql',
     dialectModule: mysql,
     benchmark: true
