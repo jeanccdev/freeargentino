1400e6dcb1bd79c737c0fe909ce703a94d87fe58
omniavincit98 <jeancc1532@gmail.com>
2023-12-25 15:54:39 -0300

    Almost finished

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     )
+    res.setHeader("Access-Control-Allow-Origin", "*")
     next()
 })
 
