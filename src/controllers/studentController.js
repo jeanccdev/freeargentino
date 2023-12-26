9e0b489148cf319a71e628b7084f5581a1752d2c
omniavincit98 <jeancc1532@gmail.com>
2023-12-26 20:35:41 -0300

    Almost finished

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -55,7 +55,7 @@ router.get('/getCpf/:cpf', async (req, res) => {
             cpf: cpf
         }
     })
-    res.send(student)
+    student ? res.send(student) : res.send(false)
 })
 
 router.post('/insertOne', authenticateToken, async (req, res) => {
